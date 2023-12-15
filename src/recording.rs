fe877c4425b842e5edce53481ac660f7bd092d21
spuschmann-tgm <shwp@protonmail.com>
2023-12-15 20:27:22 +0000

    Refactor recording logic in Recorder struct

--- src/recording.rs
+++ src/recording.rs
@@ -21,12 +21,10 @@ impl Recorder {
             unbounded();
 
         let shutdown_signal = Arc::new(AtomicBool::new(false));
+        let shutdown_signal_clone = Arc::clone(&shutdown_signal);
 
         let record_thread = {
-            let shutdown_signal_clone = Arc::clone(&shutdown_signal);
-            thread::spawn(move || {
-                Self::record_thread_logic(pcm_devices, sender, shutdown_signal_clone)
-            })
+            thread::spawn(move || record_thread_logic(pcm_devices, sender, shutdown_signal_clone))
         };
 
         let write_thread = {
@@ -41,47 +39,47 @@ impl Recorder {
             write_thread: Some(write_thread),
         })
     }
+}
 
-    fn record_thread_logic(
-        pcm_devices: [PCM; 2],
-        sender: Sender<SharedBufferMessage>,
-        shutdown_signal: Arc<AtomicBool>,
-    ) {
-        let pcm_ios = pcm_devices
-            .iter()
-            .map(|device| device.io_i16().unwrap())
-            .collect::<Vec<_>>();
+fn record_thread_logic(
+    pcm_devices: [PCM; 2],
+    sender: Sender<SharedBufferMessage>,
+    shutdown_signal: Arc<AtomicBool>,
+) {
+    let pcm_ios = pcm_devices
+        .iter()
+        .map(|device| device.io_i16().unwrap())
+        .collect::<Vec<_>>();
 
-        'outer: while !shutdown_signal.load(Ordering::SeqCst) {
-            sender.send(NewFile(new_file_name())).unwrap();
+    'outer: while !shutdown_signal.load(Ordering::SeqCst) {
+        sender.send(NewFile(new_file_name())).unwrap();
 
-            for pcm_device in pcm_devices.iter() {
-                match pcm_device.reset() {
-                    Ok(_) => {}
-                    Err(err) => {
-                        pcm_device.try_recover(err, false).unwrap();
-                        continue 'outer;
-                    }
-                }
-            }
-
-            for _ in 0..N_OF_BUFFERS_PER_FILE {
-                let data = {
-                    (
-                        get_mic_data(&pcm_devices[0], &pcm_ios[0]),
-                        get_mic_data(&pcm_devices[1], &pcm_ios[1]),
-                    )
-                };
-                match data {
-                    (Ok(a), Ok(b)) => {
-                        sender.send(Data([a, b])).unwrap();
-                    }
-                    _ => continue 'outer,
+        for pcm_device in pcm_devices.iter() {
+            match pcm_device.reset() {
+                Ok(_) => {}
+                Err(err) => {
+                    pcm_device.try_recover(err, false).unwrap();
+                    continue 'outer;
                 }
             }
         }
-        sender.send(EndThread).unwrap();
+
+        for _ in 0..N_OF_BUFFERS_PER_FILE {
+            let data = {
+                (
+                    get_mic_data(&pcm_devices[0], &pcm_ios[0]),
+                    get_mic_data(&pcm_devices[1], &pcm_ios[1]),
+                )
+            };
+            match data {
+                (Ok(a), Ok(b)) => {
+                    sender.send(Data([a, b])).unwrap();
+                }
+                _ => continue 'outer,
+            }
+        }
     }
+    sender.send(EndThread).unwrap();
 }
 
 fn get_mic_data(pcm_device: &PCM, pcm_io: &IO<'_, i16>) -> Result<Vec<i16>, Box<dyn Error>> {
