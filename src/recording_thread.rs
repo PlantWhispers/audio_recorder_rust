a8a694f35e4781e71d7cc6879ccca6260492ff37
spuschmann-tgm <shwp@protonmail.com>
2023-12-17 15:31:55 +0000

    Crazyness happend! Synced mics by estimation of mircrophones internal hardware buffer

--- src/recording_thread.rs
+++ src/recording_thread.rs
@@ -3,11 +3,11 @@ use crate::{BUFFER_SIZE, N_OF_BUFFERS_PER_FILE, SAMPLE_RATE};
 use alsa::pcm::{IO, PCM};
 use alsa::PollDescriptors;
 use crossbeam::channel::Sender;
-use libc::pollfd;
+use libc::{poll, pollfd};
 use std::error::Error;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
-use std::time::SystemTime;
+use std::time::{Instant, SystemTime};
 
 pub fn recording_thread_logic(
     pcm_devices: [PCM; 2],
@@ -35,6 +35,14 @@ pub fn recording_thread_logic(
     'outer: while !shutdown_signal.load(Ordering::SeqCst) {
         sender.send(NewFile(new_file_name())).unwrap();
 
+        let mut start_time: Option<Instant> = None;
+        let mut end_time: Option<Instant> = None;
+        enum AorB {
+            A,
+            B,
+        }
+        let mut first: Option<AorB> = None;
+
         for pcm_device in pcm_devices.iter() {
             match pcm_device.reset() {
                 Ok(_) => {}
@@ -45,16 +53,59 @@ pub fn recording_thread_logic(
             }
         }
 
-        while (unsafe { libc::poll(pds.as_mut_ptr(), 2, 1000) } != 1) {}
+        while end_time.is_none() {
+            unsafe { poll(pds.as_mut_ptr(), pds.len() as libc::nfds_t, 1000) };
+            let time = std::time::Instant::now();
 
-        let time = std::time::Instant::now();
+            match (pds[0].revents > 0, pds[1].revents > 0, &first.is_none()) {
+                (false, false, true) | (true, false, false) | (false, true, false) => continue,
+                (true, false, true) => {
+                    first = Some(AorB::A);
+                    start_time = Some(time);
+                }
+                (false, true, true) => {
+                    first = Some(AorB::B);
+                    start_time = Some(time);
+                }
+                (true, true, false) => {
+                    end_time = Some(time);
+                }
+                (true, true, true) => {
+                    panic!("Both mics started at the same time")
+                }
+                (false, false, false) => {
+                    panic!("Invalid state")
+                }
+            }
+        }
 
-        while (unsafe { libc::poll(pds.as_mut_ptr(), 2, 1000) } != 2) {}
+        let delay = match (start_time, end_time) {
+            (Some(start), Some(end)) => end.duration_since(start),
+            _ => panic!("Timing failed"),
+        };
 
-        let delay = time.elapsed();
         let frame_delay: i64 = (delay.as_secs_f64() * SAMPLE_RATE as f64).round() as i64;
 
-        println!("Delay: {:?} (~{} frames)", delay, frame_delay);
+        println!(
+            "{:?} was {:?} (~{} frames) faster",
+            match first.as_ref().unwrap() {
+                AorB::A => "Mic A",
+                AorB::B => "Mic B",
+            },
+            delay,
+            frame_delay
+        );
+
+        let mut temp_buffer = vec![0i16; frame_delay as usize];
+
+        match first.unwrap() {
+            AorB::A => {
+                pcm_ios[0].readi(&mut temp_buffer).unwrap();
+            }
+            AorB::B => {
+                pcm_ios[1].readi(&mut temp_buffer).unwrap();
+            }
+        }
 
         for _ in 0..N_OF_BUFFERS_PER_FILE {
             let data = {
