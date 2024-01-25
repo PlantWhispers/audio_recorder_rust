66b757ee0e5be6da64130d300aee95a5a87268be
spuschmann-tgm <shwp@protonmail.com>
2024-01-25 11:02:35 +0000

    Added HC-SR04

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,9 @@ const SAMPLE_RATE: u32 = 384_000;
 const CHANNELS: u16 = 1;
 const FORMAT: Format = Format::S16LE;
 const ACCESS: Access = Access::RWInterleaved;
-const ALSA_BUFFER_SIZE: usize = 19200; // Adjust as needed
+const ALSA_BUFFER_SIZE: usize = 192000; // Adjust as needed
 const BUFFER_SIZE: usize = 384; // Adjust as needed
-const TIME_BETWEEN_RESETS_IN_S: u32 = 5;
+const TIME_BETWEEN_RESETS_IN_S: u32 = 3;
 const N_OF_BUFFERS_PER_FILE: u32 = TIME_BETWEEN_RESETS_IN_S * SAMPLE_RATE / BUFFER_SIZE as u32;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
